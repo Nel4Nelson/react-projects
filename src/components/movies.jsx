50428ef7566b450adfd609b891c43d3793420f8d
Ekwueme Nelson <an.ekwueme@stu.unizik.edu.ng>
2022-08-06 15:15:38 +0100

    first prototype with delete feature implemented

---
+++ src/components/movies.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movieId) => {
+    const movies = this.state.movies.filter((m) => m._id !== movieId);
+    this.setState({ movies });
+  };
+  render() {
+    const { movies } = this.state;
+    const headerClass = "p-3 mb-2 bg-secondary text-white rounded m-2";
+    if (movies.length == 0)
+      return <h2 className={headerClass}>There are no movies in the database.</h2>;
+    return (
+      <React.Fragment>
+        {
+          <h2 className={headerClass}>
+            There are {movies.length} movies in the database.
+          </h2>
+        }
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie._id}>
+                <td scope="row">{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie._id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
